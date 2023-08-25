deb074de91267ab2ed7f601783c1b03648e606cd
Kevin Trindade Goncalves <kevin.goncalves@meta.com.br>
2023-08-25 11:32:35 -0300

    changing google btn

--- src/page/AuthPage/AuthPage.tsx
+++ src/page/AuthPage/AuthPage.tsx
@@ -1,13 +1,14 @@
-import styles from "./AuthPage.module.css";
+import style from "./AuthPage.module.css";
 import { GOOGLE_AUTH } from "../../util/constant-util";
 
 export default function Auth() {
   const authGoogle = () => window.open(GOOGLE_AUTH, "_self");
 
   return (
-    <div className={styles["container"]}>
-      <button onClick={() => authGoogle()} className={styles["google-btn"]}>
-        <img className={styles["google-img"]} alt="Google Auth" />
+    <div className={style["container"]}>
+      <button className={style["google-btn"]} onClick={() => authGoogle()}>
+        <img className={style["google-img"]} alt="Google Auth" />
+        <p>Login with Google</p>
       </button>
     </div>
   );
